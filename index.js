0718c0f5ddce439b93f4eec83fbdf6dda85afa8f
Aik123 <aik.1120ilm@gmail.com>
2022-08-28 22:04:59 +0900

    added path to db/migrations and seeds

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const { setupServer } = require("./server");
+require("dotenv").config();
+const path = require("path");
+const knex = require("knex");
+const knexfile = require("./knexfile");
+const db = knex(knexfile);
 
 const PORT = process.env.PORT || 4000;
 
-const app = setupServer();
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+const server = setupServer();
+
+// app.listen(PORT, () => {
+//   console.log("Server running on port", PORT);
+// });
+
+(async () => {
+  const dirPath = path.join(__dirname, "/db");
+  try {
+    console.log("Running migrations...");
+
+    await db.migrate.latest(dirPath);
+    await db.seed.run(dirPath);
+
+    console.log("Starting express...");
+    server.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  } catch (err) {
+    console.error("Error starting app!", err);
+    process.exit(-1);
+  }
+})();
